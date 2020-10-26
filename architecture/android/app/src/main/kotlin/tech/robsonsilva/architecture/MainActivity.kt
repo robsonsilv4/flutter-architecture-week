c1b4444a56faaf04133be36d208729792f4485fd
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-25 22:27:45 -0300

    :tada: initial project

---
+++ architecture/android/app/src/main/kotlin/tech/robsonsilva/architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package tech.robsonsilva.architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ architecture/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Architecture',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
