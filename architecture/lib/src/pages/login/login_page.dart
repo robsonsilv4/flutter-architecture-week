eb2cf6350eeb8441253a24d166c203aa3c3b5de8
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-26 17:27:04 -0300

    :sparkles: update login page

--- architecture/lib/src/pages/login/login_page.dart
+++ architecture/lib/src/pages/login/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'login_controller.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,11 +9,44 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login page'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Email",
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Password",
+              ),
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+            ),
+            SizedBox(height: 30),
+            RaisedButton(
+              onPressed: () {
+                Modular.to.pushReplacementNamed('/home');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
