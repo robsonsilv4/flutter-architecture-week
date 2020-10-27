26bfd70336e18c57f893624241d6832869d62885
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-27 11:55:38 -0300

    :construction: add search text domain

---
+++ clean_architecture/lib/modules/search/domain/entities/result_search.dart
@@ -0,0 +1,11 @@
+class ResultSearch {
+  final String title;
+  final String content;
+  final String image;
+
+  ResultSearch({
+    this.title,
+    this.content,
+    this.image,
+  });
+}
---
+++ clean_architecture/lib/modules/search/domain/repositories/search_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+
+import '../entities/result_search.dart';
+import '../errors/errors.dart';
+
+abstract class SearchRepository {
+  Future<Either<FailureSearch, List<ResultSearch>>> search(
+    String searchText,
+  );
+}
